c0f84a19019dd7603d1e5a8869f1c452164a45db
Rahul Parkar <rahul.a.parkar@gmail.com>
2017-05-02 00:10:32 +0100

    Implemented first pass of react router with home page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom'
+
+import HomeContainer from './containers/home/Home'
+
 import './App.css'
 
 class App extends Component {
   render () {
     return (
-      <div className='App'>
-        <div className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className='App-intro'>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          RAHUL WAS HERE
-        </p>
-      </div>
+      <Router>
+        <Route exact path='/' component={HomeContainer} />
+      </Router>
     )
   }
 }
