a846a35e84d2a9f8dfb1767e5621bfa644697fe0
Rahul Parkar <rahul.a.parkar@gmail.com>
2017-05-06 21:08:55 +0100

    Remove un-necessary extra div

--- src/Containers/Home/Home.js
+++ src/Containers/Home/Home.js
@@ -1,33 +1,28 @@
 import React from 'react'
-import { connect } from 'react-redux'
+import {connect} from 'react-redux'
 import TableRecentFlights from './TableRecentFlights/TableRecentFlights'
 import TableStats from './TableStats/TableStats'
 
 class Home extends React.Component {
   render () {
     return (
-      <div className='grid'>
-        <div className='contentbox'>
-          <div className='divrecentbox pad15'>
-            <div className='centerblock'>recent</div>
-            <div>
-              <TableRecentFlights recent={this.props.recent} />
-            </div>
+      <div className='grid contentbox'>
+        <div className='divrecentbox pad15'>
+          <div className='centerblock'>recent</div>
+          <div>
+            <TableRecentFlights recent={this.props.recent} />
           </div>
-          <div className='newsbox pad15'>
-            <div className='centerblock'>Number Statistics</div>
-            <TableStats general={this.props.general} />
-          </div>
+        </div>
+        <div className='newsbox pad15'>
+          <div className='centerblock'>Number Statistics</div>
+          <TableStats general={this.props.general} />
         </div>
       </div>
     )
   }
 }
 const mapStateToProps = state => {
-  return {
-    general: state.get('general'),
-    recent: state.get('recent')
-  }
+  return {general: state.get('general'), recent: state.get('recent')}
 }
 
 export default connect(mapStateToProps)(Home)
