e823e215da6858269ad7561cc3ea613bc5184c16
Daniel Hawton <daniel@hawton.com>
2017-05-06 15:22:53 -0800

    Remove unintentionally added items

--- src/index.js
+++ src/index.js
@@ -3,15 +3,10 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import './index.css'
 
-import { fetchRecent, fetchGeneral } from './actions'
-
 import configureStore from './store/configureStore'
 import Immutable from 'immutable'
 
 const store = configureStore(Immutable.Map())
-
-store.dispatch(fetchGeneral())
-store.dispatch(fetchRecent())
 
 ReactDOM.render(
   <App store={store} />,
