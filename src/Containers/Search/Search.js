395ec290de5bf2b92822b62ec006f46d57dc4f83
Rahul Parkar <rahul.a.parkar@gmail.com>
2017-05-07 01:22:43 +0100

    Added initial search page

---
+++ src/Containers/Search/Search.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {connect} from 'react-redux'
+
+class Search extends React.Component {
+  render () {
+    return (
+      <div>
+        Fuck this
+      </div>
+    )
+  }
+}
+
+export default connect()(Search)
