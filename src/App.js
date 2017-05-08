a3bcbb918faa3d410d6532e7a6aad1c698b9aca9
Daniel Hawton <daniel@hawton.com>
2017-05-07 23:36:37 -0800

    Initial static commit

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Provider } from 'react-redux'
+
+import About from './Containers/About/About'
+import Flight from './Containers/Flight/Flight'
 import Home from './Containers/Home/Home'
 import Header from './Containers/Header/Header'
-import About from './Containers/About/About'
 import Search from './Containers/Search/Search'
 import './App.scss'
 
@@ -16,6 +18,7 @@ const App = ({store}) => (
         <Route exact path='/' component={Home} />
         <Route path='/about' component={About} />
         <Route path='/search/:searchTerm' component={Search} />
+        <Route path='/flight/:id' component={Flight} />
       </div>
     </Router>
   </Provider>
---
+++ src/Containers/Flight/Flight.js
@@ -0,0 +1,92 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import {connect} from 'react-redux'
+import './Flight.scss'
+
+class Flight extends React.Component {
+  componentDidMount () {
+    // generalDispatcher = setInterval(() => this.props.dispatch(fetchGeneral()), refreshTime)
+  }
+
+  componentWillUnmount () {
+    // clearInterval(generalDispatcher)
+  }
+
+  render () {
+    return (
+      <div className='contentbox'>
+        <div className='flightinfo'>
+          <h2>ASA55</h2>
+          <h3>En-Route</h3>
+        </div>
+        <div className='flightprogress'>
+          <div className='departure'>
+            <span className='icao'>
+              PANC
+            </span>
+            <span className='name'>
+              Anchorage International
+            </span>
+          </div>
+          <div className='arrival'>
+            <span className='icao'>
+              PABR
+            </span>
+            <span className='name'>
+              Barrow
+            </span>
+          </div>
+        </div>
+        <div className='flightdetails'>
+          <div className='dividedtitle'>Flight Times</div>
+          <table>
+            <thead>
+              <th>&nbsp;</th>
+              <th>Departure</th>
+              <th>Landing</th>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Actual/<i>Estimated</i></td>
+                <td>May 7, 2017 22:17</td>
+                <td><i>May 8, 2017 02:11</i></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div className='flightdata'>
+          <div className='dividedtitle'>Flight Data</div>
+          <table>
+            <tbody>
+              <tr>
+                <td>Pilot</td>
+                <td><Link to='/search/876594'>Daniel Hawton PAFA (876594)</Link></td>
+              </tr>
+              <tr>
+                <td>Speed</td>
+                <td>401 knots</td>
+              </tr>
+              <tr>
+                <td>Altitude</td>
+                <td>33234 ft (Planned: 33000 ft)</td>
+              </tr>
+              <tr>
+                <td>Distance</td>
+                <td>Direct 1344 nm (Remaining: 344 nm)</td>
+              </tr>
+              <tr>
+                <td>Route</td>
+                <td>DCT</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    )
+  }
+}
+const mapStateToProps = state => {
+  return {general: state.get('general'), recent: state.get('recent')}
+}
+
+export default connect(mapStateToProps)(Flight)
