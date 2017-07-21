f8ec1c5c78d859f58d5c6786319efbdc5c70a8af
Daniel Hawton <daniel@hawton.com>
2017-07-20 17:48:46 -0800

    Add loading when loading flight data or position data rather than just re-rendering the DOM several times

--- src/Containers/Flight/Flight.js
+++ src/Containers/Flight/Flight.js
@@ -4,8 +4,9 @@ import {Map, Marker, Popup, TileLayer, Polyline} from 'react-leaflet'
 import {DivIcon} from 'leaflet'
 import './Flight.scss'
 import ReactGA from 'react-ga'
+import Loading from '../Loading/Loading'
 
-import { fetchFlightPosition, fetchFlightData } from '../../ducks/flight'
+import {fetchFlightPosition, fetchFlightData} from '../../ducks/flight'
 
 import FlightProgress from './FlightProgress/FlightProgress'
 import FlightDetails from './FlightDetails/FlightDetails'
@@ -14,13 +15,16 @@ import ActivityLog from './ActivityLog/ActivityLog'
 
 import planeUrl from './plane.png'
 
-let dataDispatcher, positionDispatcher
+let dataDispatcher,
+  positionDispatcher
 
 class Flight extends React.Component {
   constructor (props) {
     super(props)
 
-    this.state = {id: ''}
+    this.state = {
+      id: ''
+    }
   }
 
   componentDidMount () {
@@ -54,7 +58,9 @@ class Flight extends React.Component {
   }
 
   logDataRefresh () {
-    ReactGA.set({page: window.location.pathname + window.location.search})
+    ReactGA.set({
+      page: window.location.pathname + window.location.search
+    })
     ReactGA.event({
       category: 'Flight',
       action: 'Updated flight position',
@@ -65,25 +71,40 @@ class Flight extends React.Component {
 
   renderMap () {
     let firstPosition = this.props.flightPositions.first()
-    let bounds = [[0, 0], [90, 180]]
+    let bounds = [
+      [
+        0, 0
+      ],
+      [90, 180]
+    ]
     if (this.props.flightData.get('status') !== 'Arrived') {
       firstPosition = this.props.flightPositions.last()
     } else {
       let lastPosition = this.props.flightPositions.last()
       if (firstPosition && lastPosition) {
         bounds = [
-        [parseFloat(firstPosition.get('lat')), parseFloat(firstPosition.get('lon'))],
-        [parseFloat(lastPosition.get('lat')), parseFloat(lastPosition.get('lon'))]
+          [
+            parseFloat(firstPosition.get('lat')),
+            parseFloat(firstPosition.get('lon'))
+          ],
+          [
+            parseFloat(lastPosition.get('lat')),
+            parseFloat(lastPosition.get('lon'))
+          ]
         ]
       }
     }
 
-    const position = (firstPosition) ? [parseFloat(firstPosition.get('lat')), parseFloat(firstPosition.get('lon'))] : [51.505, -0.09]
+    const position = (firstPosition)
+      ? [
+        parseFloat(firstPosition.get('lat')),
+        parseFloat(firstPosition.get('lon'))
+      ]
+      : [51.505, -0.09]
     const map = (
       <Map center={position} maxZoom={11} zoom={6} bounds={bounds}>
         <TileLayer url='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}' attribution='Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community' />
-        <TileLayer url='https://stamen-tiles-{s}.a.ssl.fastly.net/toner-hybrid/{z}/{x}/{y}.png' attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>' />
-        {/* Re-enable if you want clickable markers */}
+        <TileLayer url='https://stamen-tiles-{s}.a.ssl.fastly.net/toner-hybrid/{z}/{x}/{y}.png' attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>' /> {/* Re-enable if you want clickable markers */}
         {/* this.renderPositions() */}
         {this.renderPolyline()}
         {this.props.flightData.get('status') !== 'Arrived' && this.renderFinalPlane()}
@@ -94,24 +115,25 @@ class Flight extends React.Component {
   }
 
   renderPositions () {
-    return this.props.flightPositions.map(
-      (position) =>
-        <Marker opacity={0} position={[parseFloat(position.get('lat')), parseFloat(position.get('lon'))]} key={`${position.get('lat')}, ${position.get('lon')}`}>
-          <Popup>
-            <div>
-              <span>Lat: {position.get('lat')}</span>
-              <br />
-              <span>Long: {position.get('lon')}</span>
-            </div>
-          </Popup>
-        </Marker>
-    )
+    return this.props.flightPositions.map((position) => <Marker opacity={0} position={[
+      parseFloat(position.get('lat')),
+      parseFloat(position.get('lon'))
+    ]} key={`${position.get('lat')}, ${position.get('lon')}`}>
+      <Popup>
+        <div>
+          <span>Lat: {position.get('lat')}</span>
+          <br />
+          <span>Long: {position.get('lon')}</span>
+        </div>
+      </Popup>
+    </Marker>)
   }
 
   renderPolyline () {
-    const positions = this.props.flightPositions.map(
-      (position) => [parseFloat(position.get('lat')), parseFloat(position.get('lon'))]
-    )
+    const positions = this.props.flightPositions.map((position) => [
+      parseFloat(position.get('lat')),
+      parseFloat(position.get('lon'))
+    ])
 
     return <Polyline positions={positions.toArray()} color='#ED8000' />
   }
@@ -124,34 +146,49 @@ class Flight extends React.Component {
       iconSize: [32, 32]
     })
 
-    return (lastPosition) ? <Marker position={[parseFloat(lastPosition.get('lat')), parseFloat(lastPosition.get('lon'))]} icon={divIcon} /> : null
+    return (lastPosition)
+      ? <Marker position={[
+        parseFloat(lastPosition.get('lat')),
+        parseFloat(lastPosition.get('lon'))
+      ]} icon={divIcon} />
+      : null
   }
 
   render () {
-    return (
-      <div className='flight'>
-        <div className='flightinfo'>
-          <h2>{this.props.flightData.get('callsign')}</h2>
-          <h3>{this.props.flightData.get('status')}</h3>
-        </div>
-        <FlightProgress flightData={this.props.flightData} />
-        <div className='flightbox'>
-          <div className='flightmap'>{this.renderMap()}</div>
-          <div className='flightstrip'>
-            <FlightDetails flightData={this.props.flightData} />
-            <FlightData flightData={this.props.flightData} />
+    if (this.props.flightIsFetching >= 1) {
+      return (
+        <Loading />
+      )
+    } else {
+      return (
+        <div className='flight'>
+          <div className='flightinfo'>
+            <h2>{this.props.flightData.get('callsign')}</h2>
+            <h3>{this.props.flightData.get('status')}</h3>
+          </div>
+          <FlightProgress flightData={this.props.flightData} />
+          <div className='flightbox'>
+            <div className='flightmap'>{this.renderMap()}</div>
+            <div className='flightstrip'>
+              <FlightDetails flightData={this.props.flightData} />
+              <FlightData flightData={this.props.flightData} />
+            </div>
+          </div>
+          <div className='history'>
+            <h2>Activity Log</h2>
+            <ActivityLog callsign={this.props.flightData.get('callsign')} />
           </div>
         </div>
-        <div className='history'>
-          <h2>Activity Log</h2>
-          <ActivityLog callsign={this.props.flightData.get('callsign')} />
-        </div>
-      </div>
-    )
+      )
+    }
   }
 }
 const mapStateToProps = state => {
-  return { flightPositions: state.getIn(['flight', 'flight', 'positions']), flightData: state.getIn(['flight', 'flight', 'flightData']) }
+  return {
+    flightPositions: state.getIn(['flight', 'flight', 'positions']),
+    flightData: state.getIn(['flight', 'flight', 'flightData']),
+    flightIsFetching: state.getIn(['flight', 'isFetching'])
+  }
 }
 
 export default connect(mapStateToProps)(Flight)
